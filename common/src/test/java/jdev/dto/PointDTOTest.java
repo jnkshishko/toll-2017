7319079d0e273050c6ff560c8f47ad2602ab492c
jnkshishko <jnkshishko@gmail.com>
2017-09-20 20:52:42 +0700

    add PointDTO, Junit

---
+++ common/src/test/java/jdev/dto/PointDTOTest.java
@@ -0,0 +1,18 @@
+package jdev.dto;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class PointDTOTest {
+
+    @Test
+    public void toJson() throws Exception {
+        PointDTO point = new PointDTO();
+        point.setLat(40);
+        point.setLon(30);
+        point.setAutoId("a012bcd");
+        assertTrue(point.toJson().contains("\"lat\":40"));
+    }
+
+}
