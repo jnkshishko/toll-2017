c2e585666b6338ed192ddefe747db0a45df79f29
jnkshishko <jnkshishko@gmail.com>
2017-10-23 13:17:11 +0700

    переписал класс WriteToFile, теперь координаты не перезаписываются, а сохраняются последовательно.

--- server-core/src/main/java/services/WriteToFile.java
+++ server-core/src/main/java/services/WriteToFile.java
@@ -13,20 +13,17 @@ public class WriteToFile {
     private static final Logger log = LoggerFactory.getLogger(WriteToFile.class);
 
     public void write(String coordinates) throws IOException {
-        File file = new File("gps.txt");
+        File file = new File("server-core/src/main/resources/gps.txt");
         ObjectMapper mapper = new ObjectMapper();
         PointDTO pointDTO = mapper.readValue(coordinates, PointDTO.class);
         FileWriter fileWriter = null;
-        BufferedWriter bufferedWriter = null;
         try {
-            fileWriter = new FileWriter(file);
-            bufferedWriter = new BufferedWriter(fileWriter);
-            bufferedWriter.write(pointDTO.toString());
-            bufferedWriter.write("\n");
+            fileWriter = new FileWriter(file, true);
+            fileWriter.append(pointDTO.toString());
+            fileWriter.append("\n");
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } finally {
-            bufferedWriter.close();
             fileWriter.close();
             log.info("Координата записана " + pointDTO.toString());
         }
