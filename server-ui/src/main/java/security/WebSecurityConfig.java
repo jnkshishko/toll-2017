544ae41cf5dad59b38a06f5ed7c34bd6dbd2406a
jnkshishko <jnkshishko@gmail.com>
2017-10-24 21:43:16 +0700

    server-ui - реализованы права доступа для различных пользователей

--- server-ui/src/main/java/security/WebSecurityConfig.java
+++ server-ui/src/main/java/security/WebSecurityConfig.java
@@ -1,7 +1,47 @@
 package security;
 
-public class WebSecurityConfig {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+
+        http
+                .authorizeRequests()
+                    .antMatchers("/img", "/css").permitAll()
+                    .antMatchers("/", "/home").authenticated()
+                    .antMatchers("/registerClient").hasRole("MANAGER")
+                    .antMatchers("/registerManager").hasRole("ROOT")
+                    .anyRequest().hasRole("CLIENT")
+                    .and()
+                .formLogin()
+                    .loginPage("/login")
+                    .permitAll()
+                    .and()
+                .logout()
+                    .permitAll();
 
 
+    }
 
+    @Autowired
+    public void configureGlobal (AuthenticationManagerBuilder auth) throws Exception {
+
+        auth
+                .inMemoryAuthentication()
+                .withUser("client").password("clientpassword").roles("CLIENT")
+                .and()
+                .withUser("manager").password("managerpassword").roles("MANAGER", "CLIENT")
+                .and()
+                .withUser("root").password("rootpassword").roles("ROOT", "CLIENT", "MANAGER");
+
+    }
 }
